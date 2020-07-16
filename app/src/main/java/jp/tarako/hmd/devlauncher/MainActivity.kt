7e95f44cde0b91adb500a070d1c183681d6715d6
hmdtarako <hmd_tarako@yahoo.co.jp>
2020-07-16 20:37:05 +0900

    開発者向けオプション起動Intentの実行

--- app/src/main/java/jp/tarako/hmd/devlauncher/MainActivity.kt
+++ app/src/main/java/jp/tarako/hmd/devlauncher/MainActivity.kt
@@ -1,11 +1,17 @@
 package jp.tarako.hmd.devlauncher
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
+        startActivity(intent)
+        finish()
     }
 }
