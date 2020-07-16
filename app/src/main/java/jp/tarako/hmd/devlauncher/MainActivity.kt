a7aa0c26c058bfe9e1a65a52a4af5fb0279b0ec3
hmdtarako <hmd_tarako@yahoo.co.jp>
2020-07-16 20:33:17 +0900

    空プロジェクトの作成

---
+++ app/src/main/java/jp/tarako/hmd/devlauncher/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.tarako.hmd.devlauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
