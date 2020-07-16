23664d87bdb162d3ef48a84ff995e693f47423bd
hmdtarako <hmd_tarako@yahoo.co.jp>
2020-07-16 23:28:51 +0900

    開発者向けオプションの状態でタイルの活性状態を変更

--- app/src/main/java/jp/tarako/hmd/devlauncher/DevLauncherTileService.kt
+++ app/src/main/java/jp/tarako/hmd/devlauncher/DevLauncherTileService.kt
@@ -2,6 +2,7 @@ package jp.tarako.hmd.devlauncher
 
 import android.content.Intent
 import android.provider.Settings
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class DevLauncherTileService : TileService() {
@@ -10,13 +11,24 @@ class DevLauncherTileService : TileService() {
         startSettings()
     }
 
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile.apply {
+            state = if (developSettingsEnabled()) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+            updateTile()
+        }
+    }
+
     private fun startSettings() {
-        val developSettingsEnabled = Settings.Secure.getInt(contentResolver, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
-        val action = if (developSettingsEnabled) Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS else Settings.ACTION_DEVICE_INFO_SETTINGS
+        val action = if (developSettingsEnabled()) Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS else Settings.ACTION_DEVICE_INFO_SETTINGS
         val intent = Intent().apply {
             setAction(action)
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
         startActivityAndCollapse(intent)
+    }
+
+    private fun developSettingsEnabled() : Boolean {
+        return Settings.Secure.getInt(contentResolver, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
     }
 }
