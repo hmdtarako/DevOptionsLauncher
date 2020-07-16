79df5e859679d8eabb8327d1c18c844834288fa2
hmdtarako <hmd_tarako@yahoo.co.jp>
2020-07-16 22:24:32 +0900

    設定画面呼び出し処理をメソッドに切り出し

--- app/src/main/java/jp/tarako/hmd/devlauncher/MainActivity.kt
+++ app/src/main/java/jp/tarako/hmd/devlauncher/MainActivity.kt
@@ -10,9 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startSettings()
+    }
+
+    private fun startSettings() {
         val developSettingsEnabled = Settings.Secure.getInt(contentResolver, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
         val action = if (developSettingsEnabled) Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS else Settings.ACTION_DEVICE_INFO_SETTINGS
-        startActivity(Intent(action))
+        val intent = Intent().apply {
+            setAction(action)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
         finish()
     }
 }
